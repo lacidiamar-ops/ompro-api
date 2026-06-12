16614b81b4c9437d6f557a318effc4202ee4fef9
lacidiamar-ops <lacidi.amar@gmail.com>
2026-06-12 14:46:29 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='ompro-v151-no-stale';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).catch(()=>new Response('Hors ligne',{status:503,statusText:'Offline'})));});
