6f0a63081bfc14360622e739d202cedcd7b43dce
lacidiamar-ops <lacidi.amar@gmail.com>
2026-06-12 14:08:24 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// OM PRO x API - service worker minimal anti-cache
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
-// Pas de stratégie cache agressive : GitHub Pages doit servir la version à jour.
